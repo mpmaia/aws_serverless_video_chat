0ad96ff3c52a39cad78149f0562fc6e3f470e4d5
Mauricio Maia <mauricio@mpmaia.com.br>
2021-07-21 16:55:47 -0300

    Adds UI auth components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Amplify} from "aws-amplify";
+import awsExports from './aws-exports';
+import {AmplifyAuthenticator, AmplifySignIn, AmplifySignOut, AmplifySignUp} from "@aws-amplify/ui-react";
+
+Amplify.configure(awsExports)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <AmplifyAuthenticator>
+          <header>
+              Amplify Login Example
+              <AmplifySignOut />
+          </header>
+          <AmplifySignUp
+              slot="sign-up"
+              usernameAlias="email"
+              formFields={[
+                  {
+                      type: "email",
+                      label: "E-mail",
+                      placeholder: "E-mail",
+                      inputProps: { required: true, autocomplete: "email" },
+                  },
+                  {
+                      type: "password",
+                      label: "Password",
+                      placeholder: "Password",
+                      inputProps: { required: true, autocomplete: "new-password" },
+                  },
+                  {
+                      type: "name",
+                      label: "Name",
+                      placeholder: "Full name",
+                  }
+              ]}
+          />
+          <AmplifySignIn slot="sign-in" usernameAlias="email" />
+      </AmplifyAuthenticator>
   );
 }
 

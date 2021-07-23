707f9523ab4f66d2a6cede999a89c0f7f056004f
Mauricio Maia <mauricio@mpmaia.com.br>
2021-07-23 16:48:08 -0300

    Authenticates hello endpoint using cognito user pool

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,72 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './App.css';
-import {Amplify} from "aws-amplify";
+import {Amplify, Auth} from "aws-amplify";
 import awsExports from './aws-exports';
 import {AmplifyAuthenticator, AmplifySignIn, AmplifySignOut, AmplifySignUp} from "@aws-amplify/ui-react";
+import axios from "axios";
+import {BASE_URL} from "./env";
 
 Amplify.configure(awsExports)
 
 function App() {
-  return (
-      <AmplifyAuthenticator>
-          <header>
-              Amplify Login Example
-              <AmplifySignOut />
-          </header>
-          <AmplifySignUp
-              slot="sign-up"
-              usernameAlias="email"
-              formFields={[
-                  {
-                      type: "email",
-                      label: "E-mail",
-                      placeholder: "E-mail",
-                      inputProps: { required: true, autocomplete: "email" },
-                  },
-                  {
-                      type: "password",
-                      label: "Password",
-                      placeholder: "Password",
-                      inputProps: { required: true, autocomplete: "new-password" },
-                  },
-                  {
-                      type: "name",
-                      label: "Name",
-                      placeholder: "Full name",
-                  }
-              ]}
-          />
-          <AmplifySignIn slot="sign-in" usernameAlias="email" />
-      </AmplifyAuthenticator>
-  );
+
+    const [hello, setHello] = useState("");
+
+    const callHello = useCallback(() => {
+        Auth.currentSession().then(session => {
+            if(session.isValid()) {
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${session.getIdToken().getJwtToken()}`,
+                    },
+                };
+                axios.get(BASE_URL + "/hello", config).then(resp => {
+                    if(resp.status===200) {
+                        setHello(resp.data.body);
+                    }
+                });
+            }
+        });
+    }, []);
+
+
+
+    return (
+          <AmplifyAuthenticator>
+              <header>
+                  Amplify Login Example
+                  <AmplifySignOut />
+              </header>
+              <pre>
+                  {hello}
+              </pre>
+              <button onClick={callHello}>Call Hello</button>
+              <AmplifySignUp
+                  slot="sign-up"
+                  usernameAlias="email"
+                  formFields={[
+                      {
+                          type: "email",
+                          label: "E-mail",
+                          placeholder: "E-mail",
+                          inputProps: { required: true, autocomplete: "email" },
+                      },
+                      {
+                          type: "password",
+                          label: "Password",
+                          placeholder: "Password",
+                          inputProps: { required: true, autocomplete: "new-password" },
+                      },
+                      {
+                          type: "name",
+                          label: "Name",
+                          placeholder: "Full name",
+                      }
+                  ]}
+              />
+              <AmplifySignIn slot="sign-in" usernameAlias="email" />
+          </AmplifyAuthenticator>
+    );
 }
 
 export default App;
